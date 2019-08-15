438a512b7d316135537e25ec48057af22f449bb6
carloscalderoon <calderoncarloos@gmail.com>
2019-08-15 18:27:43 -0400

    Heroku

--- routes.js
+++ routes.js
@@ -7,7 +7,20 @@ const superheroRoutes = require('./routes/superheroes');
 
 // Registering our pageRoutes
 // app.use('/', pageRoutes);
-app.use('/superheroes', superheroRoutes);
+//app.use('/superheroes', superheroRoutes);
+
+// Our routes
+const routes = require("./routes.js");
+app.use("/api", routes);
+
+// Handles any requests that don't match the ones above
+const root = path.join(__dirname, '/client/build');
+app.use(express.static(root));
+app.use((req, res, next) => {
+  if (req.method === 'GET' && req.accepts('html') && !req.is('json') && !req.path.includes('.')) {
+    res.sendFile('index.html', { root });
+  } else next();
+});
 
 // Exporting the changes
 module.exports = app;
